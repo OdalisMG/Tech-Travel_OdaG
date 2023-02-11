74d49a6511cfa77be70a84632d26f2d3081ff100
OdalisMG <Godalis64gmail.com>
2023-02-10 20:47:04 -0500

    10/2/2023

---
+++ tech-travel-aplication/src/pages/Home/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Title = styled.p`
+ font-size: 70 px;
+`;
+
+export const Subtitle = styled.p`
+  font-size: 10px;
+  color: #456456
+  `;
+
+
